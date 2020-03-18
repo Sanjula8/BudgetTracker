30678f1c7df832c207b1ccf47a1b1b49999490f6
Sanjula <sanjula8@gmail.com>
2020-03-18 13:35:26 -0500

    changed mongo database name

--- server.js
+++ server.js
@@ -15,11 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI =
-	process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-// routes
+// rout
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
