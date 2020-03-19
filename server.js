b1b3f3a912c82484f5efa5fdaf21b3954d2c5bc2
Sanjula <sanjula8@gmail.com>
2020-03-18 20:25:56 -0500

    adde sw and manifest file

--- server.js
+++ server.js
@@ -15,10 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/budget", {
+	useNewUrlParser: true,
+	useFindAndModify: false
+});
 
-// rout
+// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
